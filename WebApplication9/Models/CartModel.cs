6aafeeb217c162fe6e7a79900f824c1c2487c71a
Haji <tu201806253@aztu.local>
2022-01-10 12:29:21 +0400

    first

---
+++ WebApplication9/Models/CartModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication9.Models
+{
+    public class CartModel
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+
+        public int Discount { get; set; }
+
+        public DateTime date { get; set; }
+
+    }
+}
